d1ad2196d4510bed3216c123334fe2469cfbf57d
+ <+>
2023-01-24 14:07:46 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,55 +1,59 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [formData, setFormData] = useState({
-    firstName: "John",
-    lastName: "Henry",
-    admin:false,
-  });
-  // const [firstName, setFirstName] = useState("Sylvia");
-  // const [lastName, setLastName] = useState("Woods");
+  const [firstName, setFirstName] = useState("Sylvia");
+  const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
 
-  // function handleFirstNameChange(event) {
-  //   setFormData({
-  //     ...formData,
-  //     firstName:event.target.value,
-  //   });
-  // }
-
-  // function handleLastNameChange(event) {
-  //   setFormData({
-  //     ...formData,
-  //     lastName:event.target.value,
-  //   });
-  // }
-
-
-  function handleChange(event) {
-    const name = event.target.name;
-    let value = event.target.value;
-    // event.PreventDefault();
-
-    // use `checked` property of checkboxes instead of `value`
-    if (event.target.type === "checkbox") {
-      value = event.target.checked;
-    }
-
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
   }
 
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    if(firstName.length > 0) {
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+    setErrors([]);
   }
+  else {
+    setErrors(["First name is required!"]);
+  }
+}
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
+
   return (
-    <form onSubmit={handleSubmit}> 
-      <input type="text"  name="firstName "onChange={handleChange} value={formData.firstName} />
-      <input type="text" name="lastName" onChange={handleChange} value={formData.lastName} />
+    <div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    {errors.length > 0
+      ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+      : null}
+    <h3>Submissions</h3>
+    {listOfSubmissions}
+    </div>
   );
 }
 
